fbca2c614920e3d8df500f751bf89500f1d78858
Kourtney Smith <117469291+KourtneySmith@users.noreply.github.com>
2022-11-18 11:31:03 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
 // write your code here
+//Kourtney Smith J00951715
+//Ex 5.6 Pg 284
+//Nov 16, 2022
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+  int i;
+  int r;
+  int n;
+
+
+  srand(time(0));
+  r = rand() % 100;
+
+  
+cin >> n;
+ 
+  for (i = 0 ; i <= (n-1) ; i++ ) 
+
+    {cout << r << " ";} 
+
+  return 0;
+  }
